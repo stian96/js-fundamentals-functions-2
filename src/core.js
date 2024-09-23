5f16e0819b89a752c481d0214892b153d7f20a8c
Stian Rusvik <stian.rusvik@no.experis.com>
2024-09-23 11:39:29 +0200

    finished core and extension

--- src/core.js
+++ src/core.js
@@ -1,4 +1,6 @@
 // 1. Create a function named bakingTime that returns 50
+const TIME = 50
+const bakingTime = (min) => TIME
 
 // 2. Create a function named remainingBakeTime that accepts one parameter:
 // - the number of minutes your cake has been in the oven.
@@ -6,11 +8,15 @@
 // It must return how many minutes your cake has left based on your parameter
 // and the result of the bakingTime function
 
+const remainingBakeTime = (inOvenMin) => bakingTime() - inOvenMin
+
 // 3. Create a function named calculatePreparationTime that accepts one parameter:
 // - the number of layers your cake has
 //
 // It must return how many minutes it will take to prepare your cake, based on
 // each layer taking 3 minutes to prepare
+
+const calculatePreparationTime = (numLayers) => numLayers * 3
 
 // 4. Create a function named totalTimeSpent that accepts two parameters:
 // - the number of layers your cake has
@@ -18,6 +24,10 @@
 // It must return how many minutes in total you have spent making the cake,
 // which is the sum of the preparation time and the number of minutes it's been in the oven.
 // Use your calculatePreparationTime function in the calculation.
+
+const totalTimeSpent = (numLayers, minInOven) => {
+  return calculatePreparationTime(numLayers) + minInOven
+} 
 
 // Don't change the code below this line
 module.exports = {
--- src/extensions/extensions.js
+++ src/extensions/extensions.js
@@ -5,6 +5,13 @@
 // "The cake is still baking!" if there are any remaining minutes left,
 // or "You didn't set a timer!" if no value is provided to the parameter
 
+const timerStatus = (remainingMinutes) => {
+  if (remainingMinutes === undefined) {
+    return "You didn't set a timer!"
+  }
+return remainingMinutes === 0 ? `${"Phil's cake is ready!"}` : "The cake is still baking!"
+}
+
 // 2. To help Phil prepare ahead of time, create a function named estimatePrepTime
 // that accepts two parameters:
 // - an array of ingredients (e.g. ["sugar", "milk", "flour", "eggs"])
@@ -13,6 +20,10 @@
 // number of ingredients provided and the prep time per ingredient.
 // If no prep time per ingredient is provided, the function should assume each ingredient
 // takes 2 minutes to prepare
+
+const estimatePrepTime = (ingredients, prepTimeInMinutes=2) => {
+  return ingredients.length * prepTimeInMinutes
+}
 
 // 3. Phil needs to know the quantity of milk and eggs to use! Create a function
 // named calculateQuantities which accepts two parameters:
@@ -30,6 +41,13 @@
 // calculateQuantities(["milk", "eggs"], 3)
 // returns: { sugar: 0, eggs: 6 }
 
+const calculateQuantities = (ingredients, cakeLayers) => {
+  const sugar = ingredients.includes('sugar') ? 100 * cakeLayers : 0
+  const eggs = ingredients.includes('eggs') ? 2 * cakeLayers : 0
+
+  return { sugar, eggs }
+}
+
 // 4. Phil's cake is pretty small and only provides 1 portion. He wants to make a bigger one!
 // Create a function named improveRecipe that accepts two parameters:
 // - an object where the keys are ingredients and the values are quantities
@@ -42,6 +60,16 @@
 // Example:
 // improveRecipe({ eggs: 2, milk: 100, sugar: 200 }, 3)
 // returns: { eggs: 6, milk: 300, sugar: 600 }
+
+const improveRecipe = (ingredientInfo, numPortions) => {
+  const updated = {}
+
+  for (const ingredient in ingredientInfo) {
+    updated[ingredient] = ingredientInfo[ingredient] * numPortions
+  }
+
+  return updated
+}
 
 // Don't change the code below this line
 module.exports = {
